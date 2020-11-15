0e70c5b926700daf6d3a4bfe1cd4cadb7abe4682
BadKiko <kiko.games.lc@gmail.com>
2020-11-16 00:20:53 +0300

    Изменение OverLight собейта - 16.11.2020  0:20
    Перевод

---
+++ ChangeSobeit/LiquidModToOverLight.bat
@@ -0,0 +1,5 @@
+cd..
+rename d3d9_overlight.dll d3d9.dll
+rename AntTweakBar_overlight.dll AntTweakBar.dll
+rename d3d9.dll d3d9_liquid.dll 
+rename AntTweakBar.dll AntTweakBar_liquid.dll 
---
+++ ChangeSobeit/OverLightToLiquidMod.bat
@@ -0,0 +1,5 @@
+cd..
+rename d3d9.dll d3d9_overlight.dll
+rename AntTweakBar.dll AntTweakBar_overlight.dll
+rename d3d9_liquid.dll d3d9.dll
+rename AntTweakBar_liquid.dll AntTweakBar.dll
